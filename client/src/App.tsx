307056c5258c4dbbe4b94486f012894179ca0ffd
Chebupellich <chebupellich@gmail.com>
2025-03-16 23:58:44 +0400

    feat: init react project

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+    return <div>pi</div>;
+};
+
+export default App;
---
+++ client/vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@api': path.resolve(__dirname, 'src/api'),
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@styles': path.resolve(__dirname, 'src/styles'),
+            '@types': path.resolve(__dirname, 'src/types'),
+            '@utils': path.resolve(__dirname, 'src/utils'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+        },
+    },
+});
